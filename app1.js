3e1fc4471c0383be3ebecb4fcb4723303b9ba7ae
shiomi-takeshi <t.shiomi@eisansystem.jp>
2024-04-05 10:11:36 +0900

    変数埋め込み

--- app1.js
+++ app1.js
@@ -14,9 +14,34 @@ let App = function MyApp() {
   );
 };
 
+const user = {
+  name: "Hedy Lamarr",
+  imageUrl:
+    "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiIuzYaSyQlvdZYIywM3P0aOSY_ueSczlm9-HUbUvQiB3QfBLrJHo2wuBnYVhNMeGoSnIfJpMtCxgbUyU7uNxGW1zXl7yH0bppd2MCyGnwcDfvGOUFSxDi98O7lihF4EJn0tjquYxTOXmmU/s1600/online_syoudan_seisen_syokuhin.png",
+  imageSize: 90,
+};
+
+function Profile() {
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={"Photo of " + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize,
+        }}
+      />
+    </>
+  );
+}
+
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
     <App />
+    <Profile />
   </StrictMode>
 );
